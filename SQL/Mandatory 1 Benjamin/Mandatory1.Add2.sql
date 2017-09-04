66fd8ae7c9c607c7aa7bbd812c6ad3114fc6d30b
Benjamin <Benjamin>
2017-09-04 11:53:47 +0200

    nytt

---
+++ SQL/Mandatory 1 Benjamin/Mandatory1.Add2.sql
@@ -0,0 +1,7 @@
+use mandatory1;
+
+insert into customer 
+(cus_id, cus_lname, cus_fname, cus_pnumber, cus_address, cus_email)
+values 
+(77, 'Sandøy', 'Benjamin', 87654321, 'Ganktown, 1337 Blazeit 420', 'bramail@gmail.com'),
+(84, 'Kim', 'Moe', 34566782, 'Hjemm, 3715 Bestegata 7', 'kimsin@mitt.no')
