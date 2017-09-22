a17efe7f97b56bd8f88338584a2b499aacf200fb
Tønnes <tonnes.roren@gmail.com>
2017-09-22 12:33:58 +0200

    Home.java tab

--- Is-202/Sprint_1/StudentProsjektV2/src/java/Home.java
+++ Is-202/Sprint_1/StudentProsjektV2/src/java/Home.java
@@ -32,7 +32,7 @@ public class Home extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
             RequestDispatcher rd = request.getRequestDispatcher("Home.jsp");
-            rd.include(request, response);
+                rd.include(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
