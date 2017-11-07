e0470ad2aef16b4582a0a1888bb1135a6f3ab140
hyttapower <mortjm14@student.uia.no>
2017-11-07 10:23:42 +0100

    Update Upload and Download.sql

--- Is-201/SQL/Upload and Download.sql
+++ Is-201/SQL/Upload and Download.sql
@@ -7,12 +7,12 @@ create database FileDB;
 use FileDB;
  
 CREATE TABLE `contacts` (
-  `contact_id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `file_name` varchar(128) DEFAULT NULL,
+  contact_id int(11) NOT NULL AUTO_INCREMENT,
+  first_name varchar(45) DEFAULT NULL,
+  last_name varchar(45) DEFAULT NULL,
+  file_name varchar(128) DEFAULT NULL,
   `file` longblob,
-  PRIMARY KEY (`contact_id`)
+  PRIMARY KEY (contact_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Select * from `contacts` where `contact_id` ;*/
@@ -23,15 +23,19 @@ Select * from `contacts`;
 File download database. Laste ned fil som elev/forelleser laster opp. 
 */
 
-/*drop database FileDW;
+drop database FileDW;
 create database FileDW;
  
 use FileDW;
 
-Create table fileDW (
-	fileDW_id int (20) primary key null auto_increment,
-    fileDW_name varchar (150) default null,
-    fileDW_data longblob
-) Engine=InnoDB Default charset=latin1;
+CREATE TABLE `files_upload` (
+  `upload_id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(128) DEFAULT NULL,
+  `file_data` longblob,
+  PRIMARY KEY (`upload_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-Select * from fileDW;*/
+insert into `files_upload`(`file_name`, `file_data`)
+values ('test', 'fuck YOU!');
+
+Select * from fileDW;
